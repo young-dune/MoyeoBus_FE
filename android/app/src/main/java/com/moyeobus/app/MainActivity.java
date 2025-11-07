9e341c9c1344a21d94dfb044dadea707462c770f
SonYoungJun <dudwnsths1@naver.com>
2025-11-08 03:26:31 +0900

    fix: 안드로이드 웹뷰 확대 제거
    - 기존 안드로이드 웹뷰에서 폰트가 자동으로 확대되는 경우 방지
    - 실기기에서 글자 폰트가 비정상적으로 커보이던 현상 제거

--- android/app/src/main/java/com/moyeobus/app/MainActivity.java
+++ android/app/src/main/java/com/moyeobus/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.moyeobus.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebSettings settings = getBridge().getWebView().getSettings();
+    settings.setTextZoom(100);
+  }
+}
