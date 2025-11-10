12cde004d456885b9e8efe456abe9a232643dc30
SonYoungJun <dudwnsths1@naver.com>
2025-11-11 00:11:47 +0900

    feat: 네이게이션 메뉴에 필요한 아이콘 컴포넌트 추가

---
+++ src/assets/icons/HistoryIcon.tsx
@@ -0,0 +1,23 @@
+// src/icons/HistoryIcon.tsx
+export default function HistoryIcon({
+  size = 20,
+  color = "currentColor",
+}: {
+  size?: number;
+  color?: string;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM10.5 11H9V5H10.5V11Z"
+        fill={color}
+      />
+    </svg>
+  );
+}
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeIcon } from "./HomeIcon";
+export { default as HistoryIcon } from "./HistoryIcon";
+export { default as MyPageIcon } from "./MyPageIcon";
