ba0ec27c0385a82f246277a736510c8b417667c2
SonYoungJun <dudwnsths1@naver.com>
2025-12-29 01:40:09 +0900

    feat: 소셜로그인 엔드포인트 수정

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -7,13 +7,14 @@ type LoginPayload = {
 
 type Provider = "kakao" | "google";
 
-const AUTH_BASE_URL = "https://api.moyeobus.com/api/v1";
-
 export async function login(payload: LoginPayload) {
   await axiosInstance.post("/login", payload);
   console.log("로그인 성공");
 }
 
-export function loginOAuth(provider: Provider) {
-  window.location.href = `${AUTH_BASE_URL}/oauth2/authorization/${provider}`;
+export async function loginOAuth(provider: Provider) {
+  const res = await axiosInstance.post("/login/oauth", {
+    params: { provider },
+  });
+  return res;
 }
